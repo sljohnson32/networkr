147747e23b68b1ee95e1240c8bae6be58a48c8a8
Mike Ziccardi <mikeziccardi@gmail.com>
2017-01-07 14:32:22 -0700

    starts testing application current 15 passing tests

--- test/helpers/setup.js
+++ test/helpers/setup.js
@@ -7,20 +7,11 @@ require('babel-polyfill');
 global.document = require('jsdom').jsdom(`
   <head>
     <meta charset='UTF-8'>
-    <title>Shoot The Breeze</title>
+    <title>NETWORKR</title>
   </head>
   <body>
     <div id='application'></div>
   </body>
 `);
-
-// global.jsdom = jsdom.jsdom;
-// global.window = document.defaultView;
-// global.navigator = window.navigator;
-// global.XMLHttpRequest = global.window.XMLHttpRequest;
-//
-// global.sinon = require('sinon');
-// global.sinon.useFakeXMLHttpRequest();
-//
-// global.window.XMLHttpRequest = global.XMLHttpRequest;
-// global.$ = require('jquery')(global.window);
+global.window = document.defaultView
+global.navigator = window.navigator
